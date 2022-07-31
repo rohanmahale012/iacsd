23ed58f108c783f85779daa54f765c7c52225789
rohanmahale012 <rohanmahale006@gmail.com>
2022-07-31 17:43:12 +0530

    submit

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express=require ("express");
 const  app=express();
+const mysql=require("mysql");
 
 
+var con=mysql.createConnection({
+    host:"localhost:3306",
+    user:"root",
+    password:"root",
+    //port:"3306"
+    //database:"iacsdmar22"
+});
 
 
+con.connect((err)=>{
+    if (err)
+    console.log(" db connection faild ");
+
+else
+{
+    con.query("select * from emp",(err,date)=>{
+        
+        if (err)
+           console.log("no data err");
+
+           else{
+            console.log(date);
+           }
+    })
+}
+})
 
 
 app.get("/",(req,res)=>{
